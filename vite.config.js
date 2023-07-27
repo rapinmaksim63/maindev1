097d201d1739fba5f53771b78906b12103f6a411
rapinmaksim63 <rapinmaksim63@gmail.com>
2023-07-27 05:11:17 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+import http from "https";
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
+  server: { 
     proxy: {
       "/stubs": {
         "target": "https://www.smshub.org",
-        changeOrigin: true, 
+        changeOrigin: true,
+        secure: false,
+        agent: new http.Agent(),
         "headers": {
           "origin": "*"
         }
