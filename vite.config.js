02d364df4910311c23e56a45de0e22639529f03b
rapinmaksim63 <rapinmaksim63@gmail.com>
2023-07-27 03:25:21 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 80,
     proxy: {
       "/stubs": {
         "target": "https://smshub.org",
